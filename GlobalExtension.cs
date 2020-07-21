2b9cfeeab916864ac61b66c4dd5e0b4a3e172ff3
Aleksey <burovalexprogrammer@gmail.com>
2020-07-21 18:36:54 +0500

    Дизайн первого метода

---
+++ GlobalExtension.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Controls;
+
+namespace PerfomanceComparison
+{
+    public static class GlobalExtension
+    {
+        public static void Show(this Control control)
+        {
+            control.Visibility = System.Windows.Visibility.Hidden;
+        }
+        public static void Hide(this Control control)
+        {
+            control.Visibility = System.Windows.Visibility.Hidden;
+        }
+        public static void Collapse(this Control control)
+        {
+            control.Visibility = System.Windows.Visibility.Collapsed;
+        }
+    }
+}
